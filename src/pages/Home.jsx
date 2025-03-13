090c2b40c34ebe9149cead82a679fa39e08adf29
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-03-14 00:13:51 +0530

    added movie api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,38 @@
 import MovieCard from "../components/MovieCard"
 import { useState } from "react"
 import "../css/Home.css"
+import { searchMovies, getPopularMovies } from "../services/api";
+import { useEffect } from "react";
 
 function Home() {
 
     const [searchQuery, setSearchQuery] = useState("");
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-    const movies = [
-        { id: 1, title: "John Wick", release_date: "2020" },
-        { id: 2, title: "Tiger", release_date: "2025" },
-        { id: 3, title: "Columbus", release_date: "1998" }
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies);
+            }
+            catch (err) {
+                console.log(err);
+                setError("Failed to load movies...");
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        loadPopularMovies();
+    }, [])
+
+    // const movies = [
+    //     { id: 1, title: "John Wick", release_date: "2020" },
+    //     { id: 2, title: "Tiger", release_date: "2025" },
+    //     { id: 3, title: "Columbus", release_date: "1998" }
+    // ]
 
     const handleSearch = (e) => {
         e.preventDefault()
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "07485070e3a94cc7937a837ccee25bca";
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
