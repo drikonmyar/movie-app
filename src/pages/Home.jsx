0fd599cd3823da82042cc191eb180405ba651d3d
Nabyendu Ojha <nabyenduojha99@gmail.com>
2025-03-14 00:28:25 +0530

    added movie search api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,10 +34,28 @@ function Home() {
     //     { id: 3, title: "Columbus", release_date: "1998" }
     // ]
 
-    const handleSearch = (e) => {
+    const handleSearch = async (e) => {
         e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("")
+        // alert(searchQuery)
+
+        if (!searchQuery.trim()) return
+        if (loading) return
+
+        setLoading(true)
+        try {
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        }
+        catch (err) {
+            console.log(error);
+            setError("Failed to serach movies...")
+        }
+        finally {
+            setLoading(false);
+        }
+
+        // setSearchQuery("")
     }
 
     return <div className="home">
@@ -47,10 +65,14 @@ function Home() {
                 onChange={(e) => setSearchQuery(e.target.value)} />
             <button type="submit" className="search-button">Search</button>
         </form>
-
-        <div className="movies-grid">
-            {movies.map(movie => (<MovieCard movie={movie} key={movie.id} />))}
-        </div>
+        {error && <div className="error-message">{error}</div>}
+        {loading ? (
+            <div className="loading">Loading...</div>
+        ) : (
+            <div className="movies-grid">
+                {movies.map(movie => (<MovieCard movie={movie} key={movie.id} />))}
+            </div>
+        )}
     </div>
 }
 
